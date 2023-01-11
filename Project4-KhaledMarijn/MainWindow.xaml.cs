61d301fb8c16783a23da09886bc25355e17917ff
ps222876 <93656404+ps222876@users.noreply.github.com>
2023-01-11 11:11:36 +0100

    Updated

--- Project4-KhaledMarijn/MainWindow.xaml.cs
+++ Project4-KhaledMarijn/MainWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Project4_KhaledMarijn.Classes;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,11 +19,40 @@ using System.Windows.Shapes;
 
 namespace Project4_KhaledMarijn
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            PopulatePizzas();
+            DataContext = this;
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+        private ObservableCollection<Pizza> pizzas = new();
+        public ObservableCollection<Pizza> Pizzas
+        {
+            get { return pizzas; }
+            set { pizzas = value; OnPropertyChanged(); }
+        }
+        public static string serviceDeskMessage = "Something went wrong";
+        private Project4DB db = new Project4DB();
+
+        private void PopulatePizzas()
+        {
+            pizzas.Clear();
+            bool result = db.GetPizzas(pizzas);
+            if (!result)
+            {
+                MessageBox.Show(serviceDeskMessage);
+            }
         }
     }
 }
