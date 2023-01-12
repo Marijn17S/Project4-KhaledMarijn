0c809a8fb8c644a75db9c7c81906ed771c9d62e0
Marijn17S <59451866+Marijn17S@users.noreply.github.com>
2023-01-12 09:59:33 +0100

    sync

--- Project4-KhaledMarijn/Classes/Order.cs
+++ Project4-KhaledMarijn/Classes/Order.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +10,20 @@ namespace Project4_KhaledMarijn.Classes
 {
     public class Order
     {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
         public int Id { get; set; }
 
         public DateTime Date { get; set; }
 
         public Customer? User { get; set; }
 
-        /*public OrderStatus Status { get; set; }*/
+        public OrderStatus Status { get; set; }
     }
 }
