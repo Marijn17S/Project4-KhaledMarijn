a27092729648449a2a630227365ad4dfb1001452
ps222876 <ps222876@summacollege.nl>
2023-02-01 08:55:15 +0100

    unittest niet af

---
+++ Project4-KhaledMarijnTests/Classes/PizzaTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Project4_KhaledMarijn.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4_KhaledMarijn.Classes.Tests
+{
+    [TestClass()]
+    public class PizzaTests
+    {
+        [TestMethod()]
+        public void FormatPriceTest()
+        {
+            Pizza pizza = new Pizza();
+            decimal result = pizza.Price = 2.25m;
+            Assert.AreEqual("$ 25.25", result);
+            Assert.Fail();
+        }
+    }
+}
